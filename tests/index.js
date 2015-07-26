c7cda93c2def4d2e316312d7d1e5b608fc221886
Tkachenko Sergey <ts1503@yandex-team.ru>
2015-07-26 13:09:17 +0300

    hint test

--- tests/index.js
+++ tests/index.js
@@ -4,6 +4,42 @@ var fs = require('fs');
 var path = require('path');
 var exec = require('child_process').exec;
 
+//================================================================
+var assert = require('assert');
+var mongojs = require('../index');
+var db = mongojs('test', ['users']);
+
+db.users.drop()
+    .then(function () {
+        return db.users.insert([
+            {
+                name: 'first',
+                age: 23
+            }, {
+                name: 'second',
+                age: 15
+            }
+        ])
+    })
+    .then(function () {
+        return db.users.createIndex('age');
+    })
+    .then(function (indexName) {
+        assert.equal(indexName, 'age_1');
+
+        return db.users.find().hint('age_1');
+    }).then(function (res) {
+        assert.equal(res.length, 2);
+        assert.ok(res[0]._id);
+        assert.ok(res[1]._id);
+        db.close();
+    })
+		.catch(function (error) {
+			console.log(error);
+		});
+
+//================================================================
+
 var TIMEOUT = 20000;
 
 var tests = fs.readdirSync(__dirname).filter(function(file) {
